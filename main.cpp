a2cf877bb204589b86fff977e6f301828e83f126
Jakub Stypułkowski <stypulkowski.jakub@gmail.com>
2016-05-06 02:51:34 +0200

    Exercise no. 2 + final

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ string tens[] = {""," dziesiec "," dwadziescia "," trzydziesci "," czterdziesci 
 
 // prototypes
 void transform(int number);
+bool isBinary(long long int number);
+int binaryFactor(int arg);
 
 int main(int argc, char **argv) {
 	int c = atoi(argv[1]);
@@ -23,6 +25,20 @@ int main(int argc, char **argv) {
 	}
 	cout << "Wybrana liczba to " << c << endl;
 	transform(c);
+	cout << "Jesli pomnozymy argument (czyli " << c << ") przez " << binaryFactor(c) << " otrzymamy najmniejsza liczbe skladajaca sie z zer i jedynek." << endl;
+	if (binaryFactor(c)<1000001) {
+		cout << binaryFactor(c) << " to inaczej " << endl;
+		transform(binaryFactor(c));
+	}
+	else
+	cout << "Niestety jest to liczba wieksza od miliona, wiec nie mozemy jej przedstawic jako polskiego liczebnika" << endl;
+	cout << "Iloczyn tych dwoch liczb to " << (long long int)c*binaryFactor(c) << endl;
+	if ((c*binaryFactor(c))<1000001) {
+		cout << c*binaryFactor(c) << " to inaczej " << endl;
+		transform(c*binaryFactor(c));
+	}
+	else
+	cout << "Niestety jest to liczba wieksza od miliona, wiec nie mozemy jej przedstawic jako polskiego liczebnika." << endl;
 }
 
 // Number-to-text transforming function.
@@ -46,7 +62,11 @@ void transform(int numArg) {
 		if (numTab[1]==1 && numTab[2]!=0) { // if first three digits are ending with 1*
 			result += teens[numTab[2]] + thousands[9]; // any index in thousands array with "tysiecy".
 		}
-		else 
+		else
+		if (numTab[2]==0 && ((numTab[0]!=0) || (numTab[1]!=0))) {
+			result += tens[numTab[1]] + ones[numTab[2]] + thousands[9];
+		}
+		else
 		result += tens[numTab[1]] + ones[numTab[2]] + thousands[numTab[2]];
 		// Thousands done.
 		result += hundreds[numTab[3]];
@@ -59,3 +79,27 @@ void transform(int numArg) {
 		// And... it's done!
 	}
 }
+
+// Function determining if given number consists only of 1's and 0's.
+bool isBinary(long long int number) {
+	while(number > 0) {
+		if (number%10 > 1) {
+			return false;
+		}
+		else
+		number/=10;
+	}
+	return true;
+}
+
+
+// Computing factor, which multiplied by program main argument gives binary number.
+int binaryFactor (int arg) {
+	long long int k;
+	while (k>0) {
+		if(isBinary(arg*k)) break;
+		k++;
+	}
+	return k;
+}
+
